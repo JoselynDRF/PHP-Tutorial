fb4bfed1618a6272f7edf1d3041728d095a011e8
JoselynDRF <joselyndosramos@gmail.com>
2017-09-19 22:37:02 +0100

    Sintaxis basic of PHP

--- ejercicio_1/index.php
+++ ejercicio_1/index.php
@@ -1,14 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title> Ejercicio 1 </title>
+	<title> Ejercicio 1 - Sintaxis de PHP </title>
 </head>
 <body>
 	
 	<?php 
-		$message = 'Hello World';
-		echo $message;
+		$message = "Hello World!";
+
+		echo $message . "<br><br>";
+		echo "Tipo de la variable: \n" . gettype($message)  . "<br>";
+		echo "Tamaño de la variable: \n" . strlen($message) . "<br>";
+		echo "Posición de la letra W: \n" . strpos($message, "W") . "<br>";
+		echo "Mensaje cortado: \n" . substr($message, 1, 8) . "<br>";
+		echo "En UpperCase: \n" . strtolower($message) . "<br>";
+
+		// Explode
+		$words = explode(" ", $message);
+		echo $words[0] . "<br>";
+		echo $words[1] . "<br>";
+
+		// Implode
+		$array = array("Hola", "Mundo!");
+		$comma_separated = implode(" ", $array);
+		echo $comma_separated . "<br>";
+
+		// Comparison
+		$comp1 = "Hello";
+		$comp2 = "Hello";
+		$resultComp = (strcmp($comp1, $comp2) != 0) ? "Las palabras son diferentes" : "Las palabras son iguales";
+		echo $resultComp . "<br>";
+
+		// Check if variavel exists (isset)
+		$checkVar = "checkVar";
+		if (isset($checkVar)) {
+			echo "La variable $checkVar existe <br>";
+		}
+
+		// Data
+		echo "Mes actual: " . date("F") . "<br>";
+
+		// Multiplication table
+		print "<br> Tabla de multiplicación";
+		for ($i = 1; $i < 8; $i++) {
+			echo "<br>";
+			for ($j = 1; $j < 8; $j++) {
+				echo $i * $j . "\n";
+			}
+		}
 	?>
 
 </body>
