3b74e35a7629521f0f735dec3521cdc782bf6759
JoselynDRF <joselyndosramos@gmail.com>
2017-09-19 20:09:48 +0100

    "Hello World" in PHP

---
+++ ejercicio_1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Ejercicio 1 </title>
+</head>
+<body>
+	
+	<?php 
+		$message = 'Hello World';
+		echo $message;
+	?>
+
+</body>
+</html>
