3027485bde75fe32f4a64b235c59666588cbbe0d
JoselynDRF <joselyndosramos@gmail.com>
2017-09-22 20:10:02 +0100

    HTML & CSS

---
+++ pagination/index.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title> Paginación </title>
+		<link rel=stylesheet href="styles.css">
+	</head>
+	<body>
+		
+		<div class="contenedor">
+			<h1> Artículos </h1>
+			<section class="articulos">
+				<ul>
+				    <li> 1. Lorem ipsum dolor sit amet.</li>
+				    <li> 1. Lorem ipsum dolor sit amet.</li>
+				    <li> 1. Lorem ipsum dolor sit amet.</li>
+				    <li> 1. Lorem ipsum dolor sit amet.</li>
+				</ul>
+			</section>
+
+			<section class="paginacion">
+				<ul>
+				    <li class="disabled">&laquo;</li>
+				    <li class="active"><a href="#">1</a></li>
+				    <li><a href="#">2</a></li>
+				    <li><a href="#">3</a></li>
+				    <li><a href="#">4</a></li>
+				    <li><a href="#">&raquo;</a></li>
+				</ul>
+			</section>
+		</div>
+
+	</body>
+</html>
