8d8c1d92c44f8704cf97a13ffdda9557a6c51c98
JoselynDRF <joselyndosramos@gmail.com>
2017-09-22 17:13:40 +0100

    Cookies

---
+++ cookies/index.php
@@ -0,0 +1,19 @@
+<?php  
+
+	// (Nombre, valor, tiempo de vida, ubicación)
+	setcookie('font-size', '30px', time() + 60 * 60 * 24 * 30, '/');
+
+?>
+
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title> Cookies </title>
+	</head>
+	<body>
+		
+		<h1>Cookie Seteada </h1>
+
+	</body>
+</html>
--- sessions/index.php
+++ sessions/index.php
@@ -9,12 +9,11 @@
 <html lang="es">
 	<head>
 		<meta charset="UTF-8">
-		<title> Cookies y Sesiones </title>
+		<title> Sesiones </title>
 	</head>
 	<body>
 		
 		<h1> Página de inicio </h1>
-		<p></p>
 		<a href="pagina2.php"> Ir a la página 2 </a>
 
 	</body>
