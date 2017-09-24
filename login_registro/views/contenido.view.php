1abbc827abbe9b5d87de3720bcba8db77d1c6a9c
JoselynDRF <joselyndosramos@gmail.com>
2017-09-24 18:51:41 +0100

    Content page

---
+++ login_registro/views/contenido.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta name=viewport content="width=device-width,  user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link rel=stylesheet href="css/styles.css">
+		<title> Contenido </title>
+	</head>
+
+	<body>
+		<div class="contenedor">
+			<h1 class="titulo"> Contenido del sitio </h1>
+			<a href="cerrar.php"> Cerrar Sesion </a>
+			<div class="contenido">
+			<article>
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente perferendis amet voluptas atque non! Sunt id earum mollitia ab nihil quod deleniti laboriosam! Mollitia exercitationem distinctio ratione sint quis fugit?</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae iure illum doloribus quas nihil id debitis corrupti optio doloremque, sunt! Optio itaque iste modi autem praesentium illum sequi animi nostrum?</p>
+			</article>
+			</div>
+		</div>
+	</body>
+
+</html>
