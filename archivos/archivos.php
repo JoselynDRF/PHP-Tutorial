7acd8b64483abffc2b351198d37b4dbc1b5c95cb
JoselynDRF <joselyndosramos@gmail.com>
2017-09-25 17:13:28 +0100

    Read and write files

---
+++ archivos/archivos.php
@@ -0,0 +1,27 @@
+<?php  
+
+/*	$resultado = file_exists('documento.txt');
+	var_dump($resultado);
+
+	if (file_exists('documento.txt')) {
+		echo "El archivo si existe";
+	} else {
+		echo "El archivo no existe";
+	}*/
+
+	// Leer el contenido
+	echo file_get_contents('documento.txt');
+
+	// Escribir contenido
+	//file_put_contents("documento.txt", "Hola Maria \n", FILE_APPEND);
+
+/*	file_put_contents('documento.txt', '');
+	for ($i = 1; $i <= 10; $i++) {
+		file_put_contents("documento.txt", "$i \n", FILE_APPEND);
+	}*/
+
+	// Convertir en array
+	$archivo = file('documento.txt');
+	print_r($archivo);
+
+?>
