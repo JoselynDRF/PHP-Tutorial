a20f12d038b6c116b11e7a3fb009105d81aced88
JoselynDRF <joselyndosramos@gmail.com>
2017-09-27 15:27:43 +0100

    Home page finished

--- blog/views/index.view.php
+++ blog/views/index.view.php
@@ -8,29 +8,40 @@
 	<title> Blog </title>
 </head>
 <body>
-	
-	<header>
-		<div class="contenedor">
-			<div class="logo izquierda">
-				<p><a href="#"> Mi primer blog </a></p>
-			</div>
 
-			<div class="derecha">
-				<form name="busqueda" class="buscar" action="<?php echo RUTA; ?>buscar.php" method="get">
-					<input type="text" name="busqueda" placeholder="Buscar">
-					<button type="submit" class="icono fa fa-search"></button>
-				</form>
+	<?php require('header.php') ?>
 
-				<nav class="menu">
-				    <ul>
-				     	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				    	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				    	<li><a href="#"> Contacto <i class="icono fa fa-envelope"></i></a></li>
-				    </ul>
-				</nav>
-			</div>
+	<div class="contenedor">
+		<div class="post">
+			<article>
+				<h2 class="titulo"><a href="#"> Título del artículo </a></h2>
+				<p class="fecha"> 1 de enero de 2017 </p>
+				<div class="thumb">
+					<a href="#">
+						<img src="<?php echo RUTA; ?>/imagenes/1.jpg" class="img">
+					</a>
+				</div>
+				<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, cumque.</p>
+				<a href="#" class="continuar"> Continuar leyendo </a>
+			</article>
 		</div>
-	</header>
+
+		<div class="post">
+			<article>
+				<h2 class="titulo"><a href="#"> Título del artículo </a></h2>
+				<p class="fecha"> 1 de enero de 2017 </p>
+				<div class="thumb">
+					<a href="#">
+						<img src="<?php echo RUTA; ?>/imagenes/1.jpg" class="img">
+					</a>
+				</div>
+				<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, cumque.</p>
+				<a href="#" class="continuar"> Continuar leyendo </a>
+			</article>
+		</div>
+
+		<?php require('paginacion.php') ?>
+	</div>
 
 </body>
 </html>
